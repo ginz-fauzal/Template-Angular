797bea6f8c3b32c4e3b97ee6077318f77418a787
ginz-fauzal <ginz.heartnet@gmail.com>
2023-05-24 16:33:42 +0700

    add fcm token when login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
+import { Component
+,OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Renderer2 } from '@angular/core';
+import { environment } from "../../environments/environment";
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
   
   email: string="user1@example.com";
   password: string="password1";
+  token:string='';
 
   constructor(private http: HttpClient,private router: Router,private renderer: Renderer2) {
     if (localStorage.getItem('accessToken')) {
@@ -19,10 +23,31 @@ export class LoginComponent {
     }
   }
 
+  ngOnInit(): void {
+    Notification.requestPermission().then((permission) => {
+      if (permission === 'granted') {
+        this.requestPermission();
+      }
+    })
+  }
+
+  requestPermission() {
+    const messaging = getMessaging();
+    getToken(messaging, 
+     { vapidKey: environment.firebase.vapidKey}).then(
+       (currentToken) => {
+        this.token=currentToken
+     }).catch((err) => {
+        console.log('An error occurred while retrieving token. ', err);
+    });
+  }
+
   login() {
+    console.log(this.token)
     const loginData = {
       email: this.email,
-      password: this.password
+      password: this.password,
+      token:this.token
     };
 
     this.http.post('https://ardikastudio.site/template/login.php', loginData).subscribe(
